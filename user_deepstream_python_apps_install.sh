d52014d4ad94e4b81462ba03f714482e59531762
anil_kumar <anik_kumar@solomon.com.tw>
2024-06-20 17:06:40 +0800

    final update

--- user_deepstream_python_apps_install.sh
+++ user_deepstream_python_apps_install.sh
@@ -32,7 +32,7 @@ function usage() {
   echo "  -b, --build-bindings     Compile and install latest PyDS instead of downloading wheels"
   echo "  -r, --remote-branch      Specify which branch of deepstream_python_apps to clone and build from"
   echo ""
-  echo "Example: $0 --version 1.1.6"
+  echo "Example: $0 --version 1.1.11"
   echo "Example: $0 --build-bindings"
   echo "Example: $0 --build-bindings -r master"
   exit 1
@@ -58,8 +58,22 @@ apt-get update
 apt-get install -y gstreamer1.0-libav
 # apt-get install --reinstall -y gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libavresample-dev libavresample4 libavutil-dev libavutil56 libavcodec-dev libavcodec58 libavformat-dev libavformat58 libavfilter7 libde265-dev libde265-0 libx264-155 libx265-179 libvpx6 libmpeg2encpp-2.1-0 libmpeg2-4 libmpg123-0
 # apt install -y python3-gi python3-dev python3-gst-1.0 python-gi-dev git python-dev python3 python3-pip python3.8-dev cmake g++ build-essential libglib2.0-dev libglib2.0-dev-bin libgstreamer1.0-dev libtool m4 autoconf automake libgirepository1.0-dev libcairo2-dev
+echo "####################################"
+echo "Installing Gstreamer Plugins"
+echo "####################################"
+
 apt-get install --reinstall -y gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly  libswresample-dev libavutil-dev libavutil56 libavcodec-dev libavcodec58 libavformat-dev libavformat58 libavfilter7 libde265-dev libde265-0 libx265-199 libx264-163 libvpx7 libmpeg2encpp-2.1-0 libmpeg2-4 libmpg123-0
-apt install python3-gi python3-dev python3-gst-1.0 python-gi-dev git meson python3 python3-pip python3.10-dev cmake g++ build-essential libglib2.0-dev libglib2.0-dev-bin libgstreamer1.0-dev libtool m4 autoconf automake libgirepository1.0-dev libcairo2-dev
+
+echo "#####################################################################################"
+echo "Installing Gstreamer Plugins downloaded and starting to download additional libaries"
+echo "#####################################################################################"
+
+apt install -y python3-gi python3-dev python3-gst-1.0 python-gi-dev git meson python3 python3-pip python3.10-dev cmake g++ build-essential libglib2.0-dev libglib2.0-dev-bin libgstreamer1.0-dev libtool m4 autoconf automake libgirepository1.0-dev libcairo2-dev
+
+echo "#######################################################################"
+echo "Installing additional libraries finshed now installing the cuda python"
+echo "#######################################################################"
+pip3 install cuda-python
 cd /opt/nvidia/deepstream/deepstream-7.0/sources
 if [ -z "$remote_branch" ]
 then
